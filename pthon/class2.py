21a993c3bbad8931192b242dbb41ccc2978f5eba
sadafwalliyani <sadafwalliyani@gmail.com>
2022-10-10 18:48:34 +0500

    done

---
+++ pthon/class2.py
@@ -0,0 +1,24 @@
+# if 5<2:
+#     pass
+# def my_function(fname):
+#   print(fname + " Refsnes")
+
+# my_function("Emil")
+# my_function("Tobias")
+# my_function("Linus")
+
+# class Person:
+#   def __init__(self, name, age):
+#     self.name = name
+#     self.age = age
+
+# p1 = Person("John", 36)
+
+# print(p1.name)
+# print(p1.age)
+
+enternum = int(input("Enter Number:"))
+
+for i in range(1,10):
+        print(enternum,"*",i,"=",i*enternum)
+        
