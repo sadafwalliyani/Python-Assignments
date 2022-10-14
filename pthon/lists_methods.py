a56dcd13d073d15febcdde481159d668d50fdea4
sadafwalliyani <sadafwalliyani@gmail.com>
2022-10-14 17:47:17 +0500

    class-5

--- pthon/lists_methods.py
+++ pthon/lists_methods.py
@@ -1,1 +1,16 @@
+#List and Tuple
+
+#List mutable - changeable []
+#tuple imutable- Unchangeable ()
+
 #List
+from operator import index
+
+    #       0       1       2       3
+names= ['sadaf','rustam', 'Ali','samra']
+    #    -4        -3       -2     -1
+print(names[2]) #list[index]
+print(names[-1]) #List[Start:end]
+print(names) #list[start:end:seq]
+
+
