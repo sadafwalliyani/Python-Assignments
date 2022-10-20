5f80e330a4d1e802445287633cb652e175d9870a
sadafwalliyani <sadafwalliyani@gmail.com>
2022-10-20 18:26:09 +0500

    class-6

---
+++ pthon/dictionary.py
@@ -0,0 +1,11 @@
+#dictionary
+# {
+#     key:value1
+#     key:value2,
+#     key:value3
+#     ...
+# }
+#always start end with { }
+# keys and values
+# : seprators between key and values
+# , seprators between key : values pair
