e811cda62c578a88008a9880c413e706da70c30e
sadafwalliyani <sadafwalliyani@gmail.com>
2022-10-21 18:33:14 +0500

    class-6

--- pthon/dictionary.py
+++ pthon/dictionary.py
@@ -5,7 +5,8 @@
 #     key:value3
 #     ...
 # }
-#always start end with { }
+
+# always start end with { }
 # keys and values
 # : seprators between key and values
 # , seprators between key : values pair
@@ -14,4 +15,24 @@
 # we will replace indexes with keys 
 # position doesnt matter in dictionary 
 
-#
+data={
+    'name': 'Sadaf',
+    'fname':'Mansoor',
+    'age': 30,
+    'qualification': 'MED',
+    'skills': ['AI','Graphics','Cloud']
+}
+# access key value from dictionary 
+print(data['name'])
+print(data['fname'])
+
+# panda 
+
+import pandas as pd
+data2={
+'name': ['Sadaf','Sammra','Madiha'],
+'fname':['Mansoor','Asif','Saeed'],
+'Course':['AI','ML','DS']
+}
+df=pd.DataFrame(data2)
+print(df)
