3f3b40334304d9f5217b58010f74781d7d77231c
sadafwalliyani <sadafwalliyani@gmail.com>
2022-10-24 18:01:33 +0500

    class-6

---
+++ pthon/controlblocks.py
@@ -0,0 +1,60 @@
+# control blocks (control particular fucntion)
+
+# if 
+    # else
+    # elif
+    # finally
+# while
+    # break
+    # continue
+    # pass
+# for
+    # break
+    # continue
+    # pass
+# try:
+    # except
+    # finally
+    # class
+# Function 
+    # def
+
+# Reserve words
+    # https://realpython.com/lessons/reserved-keywords/
+
+
+# while Loop
+    # break
+    # continue
+    # pass
+# while Logic:
+    #statement
+    #increment/decrement
+
+print(5==10)
+
+if True:
+    print('Pakistan')
+if False:
+    print('Hindustan')
+
+
+# while
+"""* logic
+* flag = counter
+* increment/decrement
+* break
+* continue
+* pass """
+
+counter = 0
+
+while counter<=10:
+    print("Pakistan Zinda bad", counter)
+    counter+=1
+
+    counter = 100
+
+while counter>=90:
+    print("Pakistan Zinda bad", counter)
+    counter-=1
