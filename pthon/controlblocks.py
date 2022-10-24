f00d4c52ce7b7110834d1056183e1d89783a0321
sadafwalliyani <sadafwalliyani@gmail.com>
2022-10-24 18:19:35 +0500

    class-6

--- pthon/controlblocks.py
+++ pthon/controlblocks.py
@@ -55,6 +55,62 @@ while counter<=10:
 
     counter = 100
 
-while counter>=90:
+"""while counter>=90:
     print("Pakistan Zinda bad", counter)
     counter-=1
+"""
+a =1
+print(a)
+a= a+ 1 #old variable value add with new one
+print(a)
+a+=1 # old variable value add with new one
+print(a)
+
+counter = 0 #flage
+
+while counter<=10: #logic
+    print("sadaf", counter) #statement 
+    counter+=1 #increment/decrement 
+
+#break 
+counter =0
+while counter<= 1000000:
+    print('Your dead',counter)
+
+    if counter==5:
+        break
+    counter +=1  
+# break in last
+counter = 0
+while counter <= 1000000:
+    counter += 1
+    print("Pakistan zinda bad!", counter)
+    if counter == 5:
+        break  
+
+users=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+# searc_users= input('Enter use name')
+
+# counter=0
+# while counter < len(users):
+#     print(users[counter],counter)
+#     if searc_users== users[counter]:
+#         break
+#     counter+=1
+
+# counter = 0
+# while counter < len(users):    
+#     if searc_users == users[counter]:
+#         print(users[counter], counter)
+#         break
+    
+#     counter +=1
+
+code=65
+while code<=90:
+        print(code,chr(code))
+        code+=1
+
+print(chr(122))
+
