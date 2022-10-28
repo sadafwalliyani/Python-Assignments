2bedd1f9e8a2ed97e2aea2440a62c7c6f3ca8155
sadafwalliyani <sadafwalliyani@gmail.com>
2022-10-28 11:14:54 +0500

    for loop

---
+++ pthon/function.py
@@ -0,0 +1,51 @@
+"""
+FUNTIONS
+    Function name
+    arguments
+Function body
+    Set of statement
+Function Calling
+    name()
+    pass arguments
+"""
+
+"""
+PRE-DEFINE 
+USER-DEFINE
+RETURN:
+    if store output in variable ## Non-return
+    if not store output in variable ## defualt
+    not required any argument when we call ## required arguments
+    function will not work without it ## optional arguments
+    if we did'nt provide it function will also work with some defualt arguments value ## Generator function
+    generate value one by one
+    alway remember old generated value ## Lambda function
+    one line function
+    it has no name
+    one time use only
+"""
+
+"""
+RECURSIVE FUNCTION 
+    It call in own body 
+
+"""
+
+# PRE-DEFINE FUNCTION
+
+print("Helloo") 
+print(len('Hello'))
+print(id('Hello'))
+
+# RETURN/NON-RETURN FUNCTION
+
+a= len("Pakistan Zinda Baf") #None return 
+print(a) #unable to store value in variable 
+
+
+# # USER DEFINE FUNCTION 
+# def function_word(arg1, arg2,):
+#     statement1
+#     statement2
+
+# function_word(arg1, arg2)
