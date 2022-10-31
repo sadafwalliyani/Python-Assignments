abf7ba086671decb5103f5ea8d5e589156dfdbd3
sadafwalliyani <sadafwalliyani@gmail.com>
2022-10-31 18:24:03 +0500

    errors andling

--- pthon/errors_handling.py
+++ pthon/errors_handling.py
@@ -13,6 +13,7 @@ Error Types
 1=[1,2]
 print(1[7]) # try to access index 7 value
 """
+
 l = [1,2]
 print(l[0])
 
@@ -102,3 +103,113 @@ try:
     open("anc.txt")
 except (FileNotFoundError):
     print("end")
+
+"""
+Dynamic Error Handling with custome message
+"""
+print("Start")
+
+a=7
+b=0
+l=[1,2,3]
+
+try:
+    open("abc.text")
+except Exception as e:
+    print("Something Wrong",e)
+    print("End")
+
+print("Start")
+
+a=7
+b=0
+l=[1,2,3]
+
+try:
+   print(a/b)
+except Exception as e:
+    print("Something Wrong",e)
+    print("End")
+
+print("Start")
+
+a=7
+b=0
+l=[1,2,3]
+
+try:
+    print(a/b)
+    print("abc.txt")
+except Exception as e:
+    print("Something Wrong",e)
+    print("End")
+
+print("Start")
+
+a = 7
+b = 0
+l = [1,2,3]
+
+try:
+    print(a / b)
+except Exception as e:
+    print("something wrong!",e)
+else:
+    print("Run if error not occured in above statement")
+print("End")
+
+print("Start")
+
+a = 7
+b = 2
+l = [1,2,3]
+
+try:
+    print(a / b)
+except Exception as e:
+    print("something wrong!",e)
+else:
+    print("Run if error not occured in above statement")
+print("End")
+
+print("Start")
+a = 7
+b = 0
+l = [1,2,3]
+
+try:
+    print(a / b)
+except Exception as e:
+    print("something wrong!",e)
+else:
+    print("Run if error not occured in above statement")
+finally:
+    print("always run!")
+print("End")
+
+"""
+Create our own excption class
+"""
+
+class StudentCard():
+    def __init__(self,roll,name,age):
+        if age < 18 or age > 60:
+            raise Exception("not allowed less 18 or greater than 60 years old student")
+        self.roll= roll
+        self.name= name
+        self.age= age
+
+s1 = StudentCard(1,"Sadaf Mansoor",30)
+print(s1.age)
+print(s1.name)
+print(s1.roll)
+
+s2 = StudentCard(2,"Rustam",34)
+print(s2.age)
+print(s2.name)
+print(s2.roll)
+
+try:
+  print(s2=StudentCard(20,"ALI",78))
+except Exception as er:
+    print("Something Wrong",er)
