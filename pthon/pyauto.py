dc25c4cd9799c3e828e9a0cb5bf595bd5b79d5d8
sadafwalliyani <sadafwalliyani@gmail.com>
2022-11-03 18:15:31 +0500

    open opera and type pakistan

--- pthon/pyauto.py
+++ pthon/pyauto.py
@@ -1,22 +1,19 @@
 import pyautogui
 # open opera and type pakistan
-print(pyautogui.position())
 
+print(pyautogui.position())
 pyautogui.moveTo(34,1044)
 pyautogui.click() 
-# pyautogui.moveTo(591, 69,1)
-pyautogui.write('Google Chrome', interval=0.25)
+pyautogui.write('Google', interval=0.25)
 pyautogui.moveTo(303,222)
 pyautogui.click()
-pyautogui.moveTo(1104,452)
-pyautogui.click() 
-# pyautogui.moveTo(652, 171)
-# pyautogui.click() 
-# pyautogui.moveTo(602, 81,5)
-# pyautogui.click() 
-# pyautogui.write('pakistan', interval=0.5);pyautogui.press('enter')
+pyautogui.moveTo(1114,475)
+pyautogui.click()
+pyautogui.moveTo(593,79)
+pyautogui.click()
+pyautogui.write('upwork', interval=0.25)
 
-# pyautogui.click()
+
 
 # distance = 200
 # while distance > 0:
