a49b71b1295276712dbdc6dc7110dbce466f841c
sadafwalliyani <sadafwalliyani@gmail.com>
2022-11-04 17:52:22 +0500

    B-Soap

--- pthon/pyauto.py
+++ pthon/pyauto.py
@@ -1,19 +1,32 @@
 import pyautogui
-# open opera and type pakistan
-
+"""open opera and type pakistan
+"""
 print(pyautogui.position())
+
 pyautogui.moveTo(34,1044)
 pyautogui.click() 
-pyautogui.write('Google', interval=0.25)
+pyautogui.write('This Pc', interval=0.25)
 pyautogui.moveTo(303,222)
 pyautogui.click()
-pyautogui.moveTo(1114,475)
+pyautogui.moveTo(1171,582)
 pyautogui.click()
-pyautogui.moveTo(593,79)
-pyautogui.click()
-pyautogui.write('upwork', interval=0.25)
+# pyautogui.moveTo(593,79)
+# pyautogui.click()               
+# pyautogui.write('upwork', interval=0.25)
 
-
+"""
+Find position of mouse
+"""
+# import pyautogui, sys
+# print('Press Ctrl-C to quit.')
+# try:
+#     while True:
+#         x, y = pyautogui.position()
+#         positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+#         print(positionStr, end='')
+#         print('\b' * len(positionStr), end='', flush=True)
+# except KeyboardInterrupt:
+#     print('\n')
 
 # distance = 200
 # while distance > 0:
