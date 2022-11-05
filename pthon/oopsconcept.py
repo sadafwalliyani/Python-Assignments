5f46cbac02361958f741dc5fc77e48a251fecd10
sadafwalliyani <sadafwalliyani@gmail.com>
2022-11-05 15:34:21 +0500

    OOPS Concept

--- pthon/oopsconcept.py
+++ pthon/oopsconcept.py
@@ -5,4 +5,15 @@ def abc():
 print(abc())
 print(a)
 
+a=7 
+b=1
+
+def abc():
+    global a # update global variable using global key
+    a=8
+    b=20
+    return a, b
+print(abc()) # call function and return local variable value
+print(a,b)
+
 
