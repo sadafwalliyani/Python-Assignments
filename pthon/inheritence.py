94b60cefdd940ed8d97f9d24017eccaa66767ec7
sadafwalliyani <sadafwalliyani@gmail.com>
2022-11-06 18:36:52 +0500

    OOPS Concept

--- pthon/inheritence.py
+++ pthon/inheritence.py
@@ -13,3 +13,32 @@ class Parent():
         return f"{words}. language={language}"
 class Child(Parent):
             pass
+
+
+p1 =Parent()
+p1.id=1
+p1.name="Sadaf"
+p1.fname="Mansoor"
+
+print(p1.name)
+print(p1.id)
+print(p1.fname)
+print(p1.eat("Pizza"))
+print(p1.speak("Pakistan Zindabad"))
+
+
+c1 =Child()
+c1.id=1
+c1.name="Rayyan"
+c1.fname="Rustam"
+
+print(c1.name)
+print(c1.id)
+print(c1.fname)
+print(c1.eat("Pizza"))
+print(c1.speak("Pakistan Zindabad"))
+
+"""
+Inheritance example2
+add some new methods in Child class
+"""
