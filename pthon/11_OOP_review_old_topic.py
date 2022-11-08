9ab4c849d85563f54baf6a33cc65f85aae06be27
sadafwalliyani <sadafwalliyani@gmail.com>
2022-11-08 18:16:54 +0500

    OOPS Concept

--- pthon/11_OOP_review_old_topic.py
+++ pthon/11_OOP_review_old_topic.py
@@ -20,3 +20,37 @@ def abc():
 print(abc()) #call function and return local variable 
 print(a,b)
 
+a= 7 #global variable 
+b=1
+
+def abc():
+    a=8 #local variable 
+    b= 20
+    global c
+    c="Pakistan"
+    return a, b, c
+
+print(abc()) #call function and return local variable 
+print(a,b,c)
+
+
+a=7
+
+def xyz():
+    return a
+print(abc())
+print(xyz())
+
+#show error
+"""a=7 
+def abc():
+    a=8
+    global a
+    return a
+
+def xyz():
+    return
+
+print(abc())
+print(xyz())"""
+
