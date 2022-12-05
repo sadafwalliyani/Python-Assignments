423f7bfd3d23ca9800e8050bacbfde64f222d062
sadafwalliyani <sadafwalliyani@gmail.com>
2022-12-05 13:50:36 +0500

    Done

--- pthon/beautiful_soap.py
+++ pthon/beautiful_soap.py
@@ -1,29 +1,52 @@
 from bs4 import BeautifulSoup
 
 html_doc = """<html><head><title>
-The Dormouse's story
 
-The Dormouse's story
-
-
-
-Once upon a time there were three little sisters; and their names were
-
-
-Elsie,
-Lacie and
-Tillie;
-and they lived at the bottom of a well.
+<html>
+ <head>
+  <title>
+   The Dormouse's story
+  </title>
+ </head>
+ <body>
+  <p class="title">
+   <b>
+    The Dormouse's story
+   </b>
+  </p>
+  <p class="story">
+   Once upon a time there were three little sisters; and their names were
+   <a class="sister" href="http://example.com/elsie" id="link1">
+    Elsie
+   </a>
+   ,
+   <a class="sister" href="http://example.com/lacie" id="link2">
+    Lacie
+   </a>
+   and
+   <a class="sister" href="http://example.com/tillie" id="link3">
+    Tillie
+   </a>
+   ; and they lived at the bottom of a well.
+  </p>
+  <p class="story">
+   ...
+  </p>
+ </body>
+</html>
 
 ...
 </p>"""
 
 soup = BeautifulSoup(html_doc, 'html.parser')
-# print(soup)
+print(soup)
 print(soup.prettify())
 print(soup.title)
 print(soup.title.text)
 print(soup.find_all('p'))
+print(soup.find_all('a'))
+
+
 
 
 
