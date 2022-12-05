803f632b8330a274e430e75bcf29de982b7443b0
sadafwalliyani <sadafwalliyani@gmail.com>
2022-12-05 13:46:04 +0500

    Done

--- pthon/beautiful_soap.py
+++ pthon/beautiful_soap.py
@@ -1,28 +1,44 @@
 from bs4 import BeautifulSoup
 
 html_doc = """<html><head><title>
-The Dormouse's story</title></head>
-<body>
-<p class="title"><b>The Dormouse's story</b></p>
+The Dormouse's story
 
-<p class="story">Once upon a time there were three little sisters; and their names were
-<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
-<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
-<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
-and they lived at the bottom of a well.</p>
+The Dormouse's story
 
-<p class="story">...</p>"""
+
+
+Once upon a time there were three little sisters; and their names were
+
+
+Elsie,
+Lacie and
+Tillie;
+and they lived at the bottom of a well.
+
+...
+</p>"""
 
 soup = BeautifulSoup(html_doc, 'html.parser')
-
-# print(soup.prettify())
+# print(soup)
+print(soup.prettify())
 print(soup.title)
 print(soup.title.text)
-print(soup.find_all("p"))
-print([i.text for i in soup.find_all("p")])
+print(soup.find_all('p'))
 
-print(soup.p['class'])
-print(soup.find_all("a"))
 
-for link in soup.find_all('a'):
-    print(link.get('href'))
+
+
+# print(soup.prettify())
+# soup = BeautifulSoup(html_doc, 'html.parser')
+
+
+# print(soup.title)
+# print(soup.title.text)
+# print(soup.find_all("p"))
+# print([i.text for i in soup.find_all("p")])
+
+# print(soup.p['class'])
+# print(soup.find_all("a"))
+
+# for link in soup.find_all('a'):
+#     print(link.get('href'))
