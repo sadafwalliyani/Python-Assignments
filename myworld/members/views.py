846a9f11ef85567cfdc63f98295cf73026d04cb9
sadafwalliyani <sadafwalliyani@gmail.com>
2022-12-07 18:31:18 +0500

    record show on table

--- myworld/members/views.py
+++ myworld/members/views.py
@@ -9,19 +9,24 @@
     
 #     # return HttpResponse("Hello world!")
 
+# template = loader.get_template('myfirst.html')
+# return HttpResponse(template.render())
 from django.http import HttpResponse
 from django.template import loader
 from .models import Members
 
 def index(request):
   mymembers = Members.objects.all().values()
-  output = ""
-  for x in mymembers:
-    output += x["firstname"]
-  return HttpResponse(output)
+  template = loader.get_template('index.html')
+  context = {
+    'mymembers': mymembers,
+  }
+  return HttpResponse(template.render(context,request))
+  # output = ""
+  # for x in mymembers:
+  #   output += x["firstname"]
+  # return HttpResponse(output)
 
 
-  # template = loader.get_template('myfirst.html')
-  # return HttpResponse(template.render())
 
   
