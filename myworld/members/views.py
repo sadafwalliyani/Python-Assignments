e0498eeec4817b8104986e6309d086903b404a6a
sadafwalliyani <sadafwalliyani@gmail.com>
2022-12-07 18:11:11 +0500

    done

--- myworld/members/views.py
+++ myworld/members/views.py
@@ -11,7 +11,17 @@
 
 from django.http import HttpResponse
 from django.template import loader
+from .models import Members
 
 def index(request):
-  template = loader.get_template('myfirst.html')
-  return HttpResponse(template.render())
+  mymembers = Members.objects.all().values()
+  output = ""
+  for x in mymembers:
+    output += x["firstname"]
+  return HttpResponse(output)
+
+
+  # template = loader.get_template('myfirst.html')
+  # return HttpResponse(template.render())
+
+  
