3439818b33be1fe4e6bc2ea35e13fbaefb529330
sadafwalliyani <sadafwalliyani@gmail.com>
2022-12-08 18:51:45 +0500

    done

--- myworld/members/urls.py
+++ myworld/members/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('add/', views.add, name='add'),
     path('add/addrecord/', views.addrecord, name='add/record'),
     path('delete/<int:id>', views.delete, name='delete'),
+    path('update/<int:id>', views.delete, name='update'),
+    path('update/updaterecord/<int:id>', views.updaterecord, name='updaterecord'),
+
 ]
