ee9b37e13d08d59ddeee44033338ad27757cbb00
sadafwalliyani <sadafwalliyani@gmail.com>
2022-12-22 21:15:30 +0500

    done

--- djangorestapi/djangorestapi/urls.py
+++ djangorestapi/djangorestapi/urls.py
@@ -1,4 +1,4 @@
-"""todo URL Configuration
+"""djangorestapi URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from todo_api import urls as todo_urls
+# from todo_api import urls as todo_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('todos/', include(todo_urls)),
+    # path('todos/', include(todo_urls)),
 ]
