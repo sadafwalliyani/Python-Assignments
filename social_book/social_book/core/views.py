eb4924216f4c12ecca87da69fc0c613322ada6f6
sadafwalliyani <sadafwalliyani@gmail.com>
2023-01-01 17:10:45 +0500

    welcome is done

--- social_book/social_book/core/views.py
+++ social_book/social_book/core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
-def index():
+def index(request):
     return HttpResponse('<h1>Welcome tp Social book </h1>')
+
