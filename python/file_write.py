87940b94f05a0a2422f66d736deead99ef7fc7a0
sadafwalliyani <sadafwalliyani@gmail.com>
2023-01-09 18:13:39 +0500

    filewrite

--- python/file_write.py
+++ python/file_write.py
@@ -17,3 +17,26 @@ f.close()
 #open and read the file after the appending:
 f=open("demofile.txt", "r")
 print(f.read())
+
+# Create a file called "myfile.txt":
+# f = open("myfile.txt", "x")
+
+# Result: a new empty file is created!
+# Create a new file if it does not exist:
+
+f=open("myfile.txt", "w")
+
+"""Delete a File
+To delete a file, you must import 
+the OS module, and run its os.remove() function:"""
+
+# Remove the file "demofile.txt":
+# import os
+# os.remove("myfile.txt")
+
+# Check if file exists, then delete it:
+import os
+if os.path.exists("demofile.txt"):
+  os.remove("demofile.txt")
+else:
+  print("The file does not exist")
