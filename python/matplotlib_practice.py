16168491fc962bdad55fdea0ab411d1af370ee8b
sadafwalliyani <sadafwalliyani@gmail.com>
2023-01-11 22:10:33 +0500

    matplotlib

--- python/matplotlib_practice.py
+++ python/matplotlib_practice.py
@@ -6,18 +6,25 @@ import numpy as np
 
 # Matplotlib Pyplot
 
-xpoint= np.array([0,60])
-ypoint=np.array([0,250])
+# xpoint= np.array([0,60])
+# ypoint=np.array([0,250])
 
-plt.plot(xpoint,ypoint)
+# plt.plot(xpoint,ypoint)
 # print(plt.show())
 
-# Matplotlib Plotting
-# Draw a line in a diagram from position 
-# (1, 3) to position (8, 10):
+# # Matplotlib Plotting
+# # Draw a line in a diagram from position 
+# # (1, 3) to position (8, 10):
+# xpoints= np.array([1,8])
+# ypoints= np.array([3,10])
 
-xpoint=np.array([1,8])
-ypoint=np.array([3,10])
+# plt.plot(xpoints,ypoints)
+# print(plt.show())
 
-plt.plot(xpoint,ypoint)
-print(plt.show)
+# Draw two points in the diagram, one 
+# at position (1, 3) and one in position (8, 10):
+
+xpoin= np.array([1,8])
+ypoin=np.array([3,10])
+plt.plot(xpoin,ypoin, 'o')
+print(plt.show())
